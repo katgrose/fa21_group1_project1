128e1d127002d8345f46b19dbe0b59a5343270d9
James Campbell <jscampbell43@gmail.com>
2021-09-05 15:58:12 -0700

    Pixabay Story Completed: Pixabay API functionality added New Files ImageItem.java PostAdapter.java VolleySingleton.java individual_image_post.xml

---
+++ app/src/main/java/com/example/fa21_group1_project1/PostAdapter.java
@@ -0,0 +1,72 @@
+package com.example.fa21_group1_project1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostHolder> {
+
+    Context context;
+    List<ImageItem> postList;
+
+    public PostAdapter(Context context, List<ImageItem> postList){
+        this.context = context;
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mView = LayoutInflater.from(context).inflate(R.layout.individual_image_post, parent, false);
+        return new PostHolder(mView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+        ImageItem imageItem = postList.get(position);
+        holder.setImageView(imageItem.getImageUrl());
+        holder.setmTags(imageItem.getTags());
+        holder.setmLikes(imageItem.getLikes());
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    public class PostHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView mLikes, mTags;
+        View view;
+        public PostHolder(@NonNull View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+
+        public void setImageView(String url){
+            imageView = view.findViewById(R.id.imageview);
+            Glide.with(context).load(url).into(imageView);
+        }
+
+        public void setmLikes(int likes){
+            mLikes = view.findViewById(R.id.likes);
+            mLikes.setText(likes + " likes");
+        }
+
+        public void setmTags(String tag){
+            mTags = view.findViewById(R.id.tags);
+            mTags.setText(tag);
+        }
+    }
+}
