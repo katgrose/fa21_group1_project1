bee0a3df0d06454a51e997f70a17ca744a457584
katgrose <kathryngrose@gmail.com>
2021-09-10 13:21:59 -0700

    Basic User Room DB from Room tutorial

---
+++ app/src/main/java/com/example/fa21_group1_project1/data/UserRepository.kt
@@ -0,0 +1,13 @@
+package com.example.fa21_group1_project1.data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User){
+        userDao.addUser(user)
+    }
+
+}
