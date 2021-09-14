4fed6049f17f32f7a531be1e64fac0192a80d1ae
rjmorla <41564261+rjmorla@users.noreply.github.com>
2021-09-14 11:06:15 -0700

    Added unit test for login

--- app/src/test/java/com/example/fa21_group1_project1/ExampleUnitTest.java
+++ app/src/test/java/com/example/fa21_group1_project1/ExampleUnitTest.java
@@ -3,6 +3,7 @@ package com.example.fa21_group1_project1;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import androidx.room.Room;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +11,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    AccountsDatabase db = Room.databaseBuilder(getApplicationContext(), AccountsDatabase.class, "accounts_table")
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build();
+    AccountsDao accountsDao = db.AccountsDao();
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
@@ -17,7 +24,10 @@ public class ExampleUnitTest {
 
     @Test
     public void testLogin(){
-       assertTrue(MainActivity.validateUser(new Accounts("admin", "admin")));
+        Accounts accountTest = new Accounts("test1", "test2");
+        accountsDao.insertAccount(accountTest);
+        Accounts checkAccountExistTest = accountsDao.findTodoByCredentials("test1", "test2");
+        assertTrue(MainActivity.validateUser(checkAccountExistTest));
     }
 
 
